eaebde5f200e58e190eaa28570185e3248a70437
JDD_Altalis <jduvernay@altalis.fr>
2019-11-20 15:15:56 +0100

    First function to test. DAO getSecteurById

--- Model/DAO.php
+++ Model/DAO.php
@@ -17,10 +17,26 @@ class DAO{
     public function __construct($database,$server,$user,$pass)
     {
         try{
-            $this->pdo = new PDO();
+            $this->pdo = new PDO("mysql:host=$server;dbname=$database", $user, $pass);
         }
         catch (PDOException $exception){
+            die("erreur de connexion:".$exception->getMessage());
+        }
+    }
 
+    public function getSecteurById($id) : Secteur{
+        $req = "SELECT * FROM secteur WHERE id = '$id'";//sélectonne un secteur correspond à l'identification id rentrer en paramètre
+        $lignereq =($this->db)->query($req);
+        if($lignereq){
+            $result =$lignereq->fetchAll(PDO::FETCH_CLASS,'Secteur');
+            if(count($result)>0){
+                return $result[0];
+            }
+            else{
+                return new Secteur();
+            }
+        }else{
+            return new Secteur();//on renvoie un nouveaux objet utilisateur vide si la recherche dans la base de donnée à échouer
         }
     }
 
