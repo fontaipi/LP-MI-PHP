c90660533ed3347e485fe8b14fba920565edafed
JDD_Altalis <jduvernay@altalis.fr>
2019-12-22 17:28:42 +0100

    no message

--- Controler/search.controler.php
+++ Controler/search.controler.php
@@ -2,30 +2,7 @@
     include_once ("../Model/DAO.php");
     include_once ("../Model/structure.php");
 
-    if (isset($_POST['submitFiltre'])) {
-        if (isset($_POST['searchAsso']) && isset($_POST['searchEntreprise'])) {
-            $listStructures = $dao->getStructures();
 
-
-        } elseif (isset($_POST['searchEntreprise'])) {
-            $listStructures = $dao->getStructureByType(0);
-
-        } elseif (isset($_POST['searchAsso'])) {
-            $listStructures = $dao->getStructureByType(1);
-
-        }
-    } else {
-        $listStructures = $dao->getStructures();
-    }
-    $listeAffichage = [];
-    foreach ($listStructures as $structure){
-        $element = [];
-        $secteurID = $dao->getSecteursStructuresByStructureID($structure->ID);
-            $secteur  = $dao->getSecteurById($secteurID->ID);
-            array_push($element,$structure);
-            array_push($element,$secteur);
-        array_push($listeAffichage,$element);
-    }
 
 if (isset($_POST['submit'])) {
     if (isset($_POST['idStructure'])) {$id = $_POST['idStructure'];}
@@ -65,4 +42,28 @@ if (isset($_POST['submitDelete'])){
     }
 }
 
+if (isset($_POST['submitFiltre'])) {
+    if (isset($_POST['searchAsso']) && isset($_POST['searchEntreprise'])) {
+        $listStructures = $dao->getStructures();
+
+
+    } elseif (isset($_POST['searchEntreprise'])) {
+        $listStructures = $dao->getStructureByType(0);
+
+    } elseif (isset($_POST['searchAsso'])) {
+        $listStructures = $dao->getStructureByType(1);
+
+    }
+} else {
+    $listStructures = $dao->getStructures();
+}
+$listeAffichage = [];
+foreach ($listStructures as $structure){
+    $element = [];
+    $secteurID = $dao->getSecteursStructuresByStructureID($structure->ID);
+    $secteur  = $dao->getSecteurById($secteurID->ID);
+    array_push($element,$structure);
+    array_push($element,$secteur);
+    array_push($listeAffichage,$element);
+}
     include_once ("../View/search.view.php");
